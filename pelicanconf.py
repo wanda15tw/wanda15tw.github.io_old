9b567a8f04b99d9976d22877020e8417b60bd68b
Wanda Juan <dotdotpanda@gmail.com>
2019-11-23 17:02:42 -0500

    modified config to add theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,9 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+
+# Theme
+THEME = "~/pelican-themes/mnmlist"
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
